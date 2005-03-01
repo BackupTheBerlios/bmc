df23e22f529b61df6aaca743a2deeb4519955433
raduprv <raduprv>
2005-03-01 17:55:23 +0000

    *** empty log message ***

--- bmc/manufacture.h
+++
@@ -1,9 +0,0 @@
-#ifndef __MANUFACTURE_H__
-#define __MANUFACTURE_H__
-
-extern int manufacture_win;
-void build_manufacture_list();
-void display_manufacture_menu();
-//int check_manufacture_interface();
-
-#endif
